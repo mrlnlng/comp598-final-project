7aa188a9eb28137a6ffa09c4f00a80aa06bd650e
pyadollahi <pyadollahi@coveo.com>
2021-12-03 15:37:58 -0500

    Write to CSV - sentiment

--- src/sentiment.py
+++ src/sentiment.py
@@ -2,7 +2,7 @@ from pandas.core.frame import DataFrame
 from textblob import TextBlob
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
 
-from clean_data import read_tsv
+from clean_data import create_output_CSV, read_tsv
 
 # First time running: uncomment this and run it
 # nltk.download('vader_lexicon')
@@ -43,6 +43,30 @@ def main():
   print('Positive tweets: {}'.format(len(positive_list)))
   print('Negative tweets: {}'.format(len(negative_list)))
   print('Neutral tweets: {}'.format(len(neutral_list)))
+
+  add_to_tsv(neutral_list, negative_list, positive_list)
+
+
+def add_to_tsv(pos: list[str], neg: list[str], neu: list[str]):
+  fpath: str = './../data/clean_data.tsv'
+  file_output: str = '../data/clean_data_labeled.tsv'
+  # fpath_output_dir: str = '../data/'
+  df: DataFrame = read_tsv(fpath=fpath)
+  df['status'] = None
+
+  for tweet in pos:
+    row_index = df[df['text'] == tweet].index
+    df.loc[row_index, 'staus'] = 'pos'
+
+  for tweet in neg:
+    row_index = df[df['text'] == tweet].index
+    df.loc[row_index, 'staus'] = 'neg'
+
+  for tweet in neu:
+    row_index = df[df['text'] == tweet].index
+    df.loc[row_index, 'staus'] = 'neu'
+
+  create_output_CSV(df=df, fpath=file_output, sep='\t')
 
 if __name__ == '__main__':
   main()
